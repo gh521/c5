c3385fc41a225fa1d8e5e20e03e0a2a94317c389
xx <qq@a.com>
2019-01-18 10:02:14 +0800

    结够框架和添加功能

--- main.c
+++ main.c
@@ -1,9 +1,49 @@
 #include <stdio.h>
+#include <math.h>
+struct point
+{
+    int x;
+    int y;
+};
 
-int main(){
-
-  
-
-    return 0;
-
+int main()
+{
+    int index = 0;
+    struct point arr[100];
+    printf("huanyinjinrucheliangguanlixitong\n");
+    while (1)
+    {
+        printf("1---tianjicheliang\n");
+        printf("2----shanchucheliang\n");
+        printf("3----xianshiquanbuchelian\n");
+        printf("4----fanhuiliwozuijindecheliang\n");
+        printf("5----tuichu");
+        printf("qinxuanzhexiangyingongneng\n");
+        int coude;
+        scanf("%d", &coude);
+        if (coude == 1)
+        {
+            int x;
+            int y;
+            printf("tinajiayigecheliangweiz\n");
+            scanf("%d", &x);
+            scanf("%d", &y);
+            char xx;
+            scanf("%c", &xx);
+            scanf("%c", &xx);
+            printf("chenggongdianjihuichejixu\n");
+        }
+        if (coude == 2)
+        {
+        }
+        if (coude == 3)
+        {
+        }
+        if (coude == 4)
+        {
+        }
+        if (coude == 5)
+        {
+        }
+    }
 }
